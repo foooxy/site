04765939970d48ce3cf5a6c4408c38c861cd80ed
foxy <xxx733789@yandex.ru>
2016-10-16 22:35:21 +0300

    Add some admin features

--- index.php
+++ index.php
@@ -79,8 +79,8 @@
 			'rows'=>$rows));
 	});
 
-	$klein->respond('GET', '/admin', function() use ($twig){
-		$query="SELECT p.id, pp.value, p.name AS productName, cn.name AS category, params.name, params.type
+	$klein->respond('GET', '/admin', function($request) use ($twig){
+		$query="SELECT p.id, pp.id AS ppId, pp.value, p.name AS productName, p.alias, cn.name AS category, params.name, params.type
 				FROM productparams AS pp
 				LEFT JOIN products AS p
 				ON pp.product = p.id
@@ -92,26 +92,20 @@
 				ON cat.param = params.id
 				ORDER BY pp.id";
 		$res = mysql_query($query);
-		$currentId = -1;
 		$items = array();
 		$params = array();
 		$productName = '';
 		$category = '';
+		$alias = '';
+		$paramId = -1;
 		while($row = mysql_fetch_array($res)){
 			$productName = $row['productName'];
 			$category = $row['category'];
-			if ($currentId != $row['id']){
-				$currentId = $row['id'];
-				if(count($params) > 0){
-					$items[] = array(
-						'id'=>$row['id'],
-						'productName'=>$row['productName'],
-						'category'=>$row['category'],
-						'params'=>$params
-						);
-				}
-			}
+			$alias = $row['alias'];
+			$paramId = $row['ppId'];
+
 			$params[] = array(
+						'id'=>$row['ppId'],
 						'name'=>$row['name'],
 						'type'=>$row['type'],
 						'value'=>$row['value']
@@ -119,17 +113,111 @@
 		}
 		if(count($params) > 0){
 			$items[] = array(
-				'id'=>$currentId,
+				'id'=>$paramId,
 				'productName'=>$productName,
 				'category'=>$category,
+				'alias'=>$alias,
 				'params'=>$params
 				);
 		}
 
+		if($request->param('reload', -1) == -1){
+			echo $twig->render('admin.html', array(
+				'items'=>$items
+			));
+		} else {
+			echo $twig->render('adminReload.html', array(
+				'items'=>$items
+				));
+		}	
+	});
+	$klein->respond('GET', '/admin/products/editParam/[:id]', function($request) use ($twig){
+		$query = "SELECT pp.value, param.name AS paramName, param.type
+		FROM productparams AS pp
+		LEFT JOIN categories AS c
+		ON pp.cat = c.id
+		LEFT JOIN params AS param
+		ON c.param = param.id
+		WHERE pp.id=".$request->id;
+		$res = mysql_query($query);
+		$row = mysql_fetch_array($res);
 
-		echo $twig->render('admin.html', array(
-			'items'=>$items
-			));	
+		echo $twig->render('editParam.html', array(
+			'param'=>$row['paramName'],
+			'value'=>$row['value'],
+			'type'=>$row['type']
+			));
+	});
+	$klein->respond('GET', '/admin/products/addProduct', function($request) use ($twig){
+		if($request->param('pn', -1) == -1){
+			$query = 'SELECT name, alias FROM catnames';
+			$res = mysql_query($query);
+			while($cat = mysql_fetch_array($res)){
+				$cats[] = $cat;
+			}
+
+			echo $twig->render('editProduct.html', array(
+				'productName'=>'',
+				'alias'=>'',
+				'cats'=>$cats
+				));
+		} else {
+			$query = "INSERT INTO products (name, alias) VALUES ('".$request->param('pn')."', '".$request->param('a')."')";
+			$res = mysql_query($query);
+			echo mysql_insert_id();
+		}
+	});
+
+	$klein->respond('GET', '/admin/products/editProduct', function($request) use ($twig){
+		if($request->param('pn', -1) == -1){
+			$query = "SELECT p.name AS productName, p.alias, cn.alias AS catAlias 
+			FROM productparams AS pp
+			LEFT JOIN products AS p
+			ON pp.product = p.id
+			LEFT JOIN categories AS c
+			ON pp.cat = c.id
+			LEFT JOIN catnames AS cn
+			ON c.cat = cn.id
+			WHERE pp.id=".$request->param('id', -1);
+			$res = mysql_query($query);
+			$row = mysql_fetch_array($res);
+			
+			$query = 'SELECT name, alias FROM catnames';
+			$res = mysql_query($query);
+			while($cat = mysql_fetch_array($res)){
+				if($cat['alias'] == $row['catAlias']){
+					$cat['selected'] = '1';
+				} else {
+					$cat['selected'] = '0';
+	 			}
+				$cats[] = $cat;
+			}
+
+			echo $twig->render('editProduct.html', array(
+				'productName'=>$row['productName'],
+				'alias'=>$row['alias'],
+				'cats'=>$cats
+				));
+		} else {
+			$query = "SELECT c.id 
+			FROM categories AS c
+			LEFT JOIN catnames AS cn
+			ON c.cat = cn.id 
+			WHERE cn.alias='".$request->param('c')."'";
+			$res = mysql_query($query);
+			$cat = mysql_fetch_array($res);
+			$query = "UPDATE productparams AS pp, products AS p
+			SET p.name='".$request->param('pn')."', p.alias='".$request->param('a')."', pp.cat='".$cat['id']."' 
+			WHERE p.id = pp.product AND pp.id=".$request->param('id', -1);
+			$res = mysql_query($query);
+			echo $res;
+		}
+	});
+	$klein->respond('GET', '/admin/products/editParam', function($request) use ($twig){
+		
+		$query = "UPDATE productparams AS pp, params AS p, categories AS c SET p.name = '".$request->param('p')."', p.type = '".$request->param('t')."', pp.value = '".$request->param('v')."' WHERE pp.cat = c.id AND c.param = p.id AND pp.id=".$request->param('id');
+		$res = mysql_query($query);
+		echo $res;	
 	});
 	$klein->dispatch();
 ?>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,99 @@
-$('.collapse').on('show.bs.collapse', function () {
-    $('.collapse.in').collapse('hide');
+$(document).ready(function(){
+	$('#addProduct').on('shown.bs.modal', function(event){
+		$.get('/admin/products/addProduct', function(data){
+			$('#addProductResult').html(data);
+		});
+	});
+	$('#submitAddProduct').click(function(e){
+		var productName = $('#addProductResult #productName').val();
+		var alias = $('#addProductResult #alias').val();
+		var category = $('#addProductResult #category option:selected').val();	
+		$.get('/admin/products/addProduct?pn=' + productName
+			+ '&a=' + alias
+			+ '&c=' + category,
+		function(data){
+			alert(data);
+			if(data > 0){
+				$.ajax({
+					url: '/admin?reload=1',
+					cached: false,
+					success: function(data){
+						$('.productsTable').html(data);
+					}
+				});
+			}
+			$('#addProduct').modal('hide');
+		});
+	});
+
+	$('#editProduct').on('shown.bs.modal', function(event){
+		var button = $(event.relatedTarget);
+		var id = button.data('whatever');
+		button.attr('data-selected', 'true');
+		$.get('/admin/products/editProduct?id=' + id, function(data){
+			$('#productResult').html(data);
+		});
+	});
+	$('#editProduct').on('hidden.bs.modal', function(event){
+		var button = $('#editProductBtn[data-selected = true]');
+		button.attr('data-selected', 'false');
+	});
+	$('#submitProduct').click(function(e){
+		var id = $('#editProductBtn[data-selected = true]').data('whatever');
+		var productName = $('#editProduct #productName').val();
+		var alias = $('#editProduct #alias').val();
+		var category = $('#editProduct #category option:selected').val();
+		$.get('/admin/products/editProduct?id=' + id
+			+ '&pn=' + productName
+			+ '&a=' + alias
+			+ '&c=' + category,
+		function(data){
+			if(data > 0){
+				$.ajax({
+					url: '/admin?reload=1',
+					cache: false,
+					success: function(data){
+						$('.productsTable').html(data);
+					}
+				});	
+			}
+			$('#editProduct').modal('hide');
+		});
+	});
+
+	$('#editParam').on('shown.bs.modal', function(event){
+		var button = $(event.relatedTarget);
+		var id = button.data('whatever');
+		button.attr('data-selected', 'true');
+		$.get('/admin/products/editParam/' + id, function(data){
+			$('#paramResult').html(data);
+		});
+	});
+	$('#editParam').on('hidden.bs.modal', function(event){
+		var button = $('#editParamBtn[data-selected = true]');
+		button.attr('data-selected', 'false');
+	});
+	$('#submitParam').click(function(e){
+		var id = $('#editParamBtn[data-selected = true]').data('whatever');
+		var param = $('#param').val();
+		var value = $('#value').val();
+		var type = $('#type').val();
+
+		$.get('/admin/products/editParam?id=' + id
+			+ '&p=' + param
+			+ '&v=' + value
+			+ '&t=' + type,
+		function(data){
+			if(data > 0){
+				$.ajax({
+					url: '/admin?reload=1',
+					cache: false,
+					success: function(data){
+						$('.productsTable').html(data);
+					}
+				});
+			}
+			$('#editParam').modal('hide');	
+		});
+	});
 });
